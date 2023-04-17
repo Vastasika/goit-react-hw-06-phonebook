e4ff0bc03cf54d7d9a71e65465c05de936f4e233
Vastasika <kykysia13@gmail.com>
2023-04-17 19:07:50 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { Form } from './Form/Form';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+
+import css from './Form/Form.module.css';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.app}>
+      <h1>Phone book</h1>
+      <Form />
+      <h2>Contacts</h2>
+      <div className={css.contactsWrap}>
+        <Filter />
+        <ContactList />
+      </div>
+      <ToastContainer />
     </div>
   );
 };
